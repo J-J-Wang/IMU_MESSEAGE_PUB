912d16ca175739b5177e52ef1479ffbf1ac20cb7
J-Wang <798959682@qq.com>
2020-10-18 14:35:54 +0800

    second

--- imu_pub/script/imu_pub.py
+++ imu_pub/script/imu_pub.py
@@ -27,67 +27,72 @@ data=str(binascii.b2a_hex(s.read(6)))
 print(data)
 print('SET 100Hz OUTPUT FREQUNSE')
  
-while not rospy.is_shutdown():
-    data= str(binascii.b2a_hex(s.read(48)))
-    g_value=data[26:44]
-    q_value=data[62:94]
-    print(data)
-    print(g_value)
-    print(q_value)
-    #x_acc   
-    print(g_value[1:6])
-    x_acc=int(g_value[1:6])/10000.0
-    print(x_acc)
-    if int(g_value[0]):
-        x_acc=-1*x_acc
-    #y_acc 
-    y_acc=int(g_value[7:12])/10000.0
-    if int(g_value[6]) :
-        y_acc=-1*y_acc
-    #z_acc  
-    z_acc=int(g_value[13:])/10000.0
-    if int(g_value[12]):
-        z_acc=-1*z_acc
-    print(z_acc)
+try:
+    while not rospy.is_shutdown():
+        data= str(binascii.b2a_hex(s.read(48)))
+        g_value=data[26:44]
+        a_value=data[44:62]
+        print(data)
+        print(g_value)
+        print(a_value)
  
-    #q1  
-    q1=int(q_value[1:9])/1000000.0
-    if int(q_value[0]):
-        q1=-1*q1
+        #x_acc   
+        x_acc=9.87933*int(g_value[1:6])/10000.0
+        if int(g_value[0]):
+            x_acc=-1*x_acc
  
-    #q2  
-    q2=int(q_value[9:17])/1000000.0
-    if int(q_value[8]):
-        q2=-1*q2
+        #y_acc 
+        y_acc=9.87933*int(g_value[7:12])/10000.0
+        if int(g_value[6]) :
+            y_acc=-1*y_acc
  
- 
-    #q3
-    q3=int(q_value[17:24])/1000000.0
-    if int(q_value[16]):
-        q3=-1*q3
- 
-    #q4   
-    q4=int(q_value[25:])/1000000.0
-    if int(q_value[24]):
-        q4=-1*q4
-    print(q4)
-  
-    imuMsg = Imu()
-    stamp = rospy.get_rostime()
-    imuMsg.header.stamp, imuMsg.header.frame_id = stamp, "imu_link"
-    #imuMsg.orientation = eul_to_qua(ef_l)
-    imuMsg.orientation.x=q1
-    imuMsg.orientation.y=q2
-    imuMsg.orientation.z=q3
-    imuMsg.orientation.w=q4
- 
+        #z_acc  
+        z_acc=9.87933*int(g_value[13:])/10000.0
+        if int(g_value[12]):
+            z_acc=-1*z_acc
     
-    #imuMsg.orientation_covariance = cov_orientation
-    #imuMsg.angular_velocity.x, imuMsg.angular_velocity.y, imuMsg.angular_velocity.z = x_ang_acc, y_ang_acc, z_ang_acc
-    #imuMsg.angular_velocity_covariance = cov_angular_velocity
-    imuMsg.linear_acceleration.x, imuMsg.linear_acceleration.y, imuMsg.linear_acceleration.z = x_acc,y_acc,z_acc
-    #imuMsg.linear_acceleration_covariance = cov_linear_acceleration
-    imuPub.publish(imuMsg)
-    rate.sleep()
+ 
+        #an_x  
+        an_x=int(a_value[1:6])/100.0/57.29578
+        if int(a_value[0]):
+            an_x=-1*an_x
+ 
+        #an_y
+        an_y=int(a_value[7:12])/100.0/57.29578
+        if int(a_value[6]):
+            an_y=-1*an_y
+ 
+        #an_z
+        an_z=int(a_value[13:])/100.0/57.29578
+        if int(a_value[12]):
+            an_z=-1*an_z
+ 
+        print('acc:x,y,z:',x_acc,y_acc,z_acc)
+        print('an_acc:x,y,z:',an_x,an_y,an_z)
+    
+        imuMsg = Imu()
+        stamp = rospy.get_rostime()
+        imuMsg.header.stamp, imuMsg.header.frame_id = stamp, "imu_link"
+        #imuMsg.orientation = eul_to_qua(ef_l)
+        #imuMsg.orientation.x=q1
+        #imuMsg.orientation.y=q2
+        #imuMsg.orientation.z=q3
+        #imuMsg.orientation.w=q4
+ 
+        
+        #imuMsg.orientation_covariance = cov_orientation
+        imuMsg.angular_velocity.x, imuMsg.angular_velocity.y, imuMsg.angular_velocity.z = an_x,an_y,an_z
+        #imuMsg.angular_velocity_covariance = cov_angular_velocity
+        imuMsg.linear_acceleration.x, imuMsg.linear_acceleration.y, imuMsg.linear_acceleration.z = x_acc,y_acc,z_acc
+        #imuMsg.linear_acceleration_covariance = cov_linear_acceleration
+        imuPub.publish(imuMsg)
+        rate.sleep()
+except:
+    Hex_str=codecs.decode('7705000C0011','hex')
+    s.write(Hex_str)
+    data= str(binascii.b2a_hex(s.read(6)))
+    print('ANSER TYPE SETTING SUCCESS')
+    pass
+ 
  
     
